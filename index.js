209232abf031278c99cebf627c5706bdd1d14cad
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-19 12:37:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName='bob'
+var bestCustomer='not bob'
+const leastFavoriteCustomer='definetly bob'
+function upperCaseCustomerName(customerName){
+return (customerName.toUpperCase())
+}
+
+
+function setBestCustomer(bestCustomer){
+    console.log(bestCustomer)
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+    console.log(bestCustomer)
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='maybe Sam'
+    console.log(leastFavoriteCustomer)
+}
+
